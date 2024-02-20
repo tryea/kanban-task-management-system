714c282de8f7a7dd033ab72049f33ed65f897dc0
Ersapta Aristo <aristoersapta@gmail.com>
2024-02-20 10:10:09 +0700

    [main] feat:Add Header Navigation

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -1,3 +1,5 @@
 import SidebarOpenIconSVG from "@/../public/icons/sidebar-open.svg";
+import TrippleDotIconSVG from "@/../public/icons/tripple-dot.svg";
 
 export const SidebarOpenIcon = SidebarOpenIconSVG;
+export const TrippleDotIcon = TrippleDotIconSVG;
--- src/components/organisms/HeaderNav.tsx
+++ src/components/organisms/HeaderNav.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { TrippleDotIcon } from "../icons/icons";
 
 type Props = {};
 
 function HeaderNav(props: Props) {
-  return <div>HeaderNav</div>;
+  return (
+    <div
+      className={`flex h-24 w-full flex-row items-center justify-between bg-kanban-white px-6 dark:bg-kanban-darkGray`}
+    >
+      <div className={`select-none heading-xl`}>
+        <p>Platform Launch</p>
+      </div>
+      <div className="flex flex-row items-center gap-6">
+        <div>
+          <button
+            type="button"
+            className={`rounded-full bg-kanban-purple px-6 py-3.5 text-kanban-white heading-m hover:bg-kanban-lightPurple dark:hover:bg-kanban-purple/65`}
+          >
+            + Add New Task
+          </button>
+        </div>
+        <div>
+          <TrippleDotIcon
+            className={`aspect-auto h-5 cursor-pointer fill-current text-kanban-grayBlue`}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderNav;
